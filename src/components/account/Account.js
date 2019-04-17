19f81a581908d4fdcac0f06ed96e9db7ceb10423
FrontDevJacob <jxtoxi24@gmail.com>
2019-04-17 21:22:32 +0200

    profile layout done

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -4,6 +4,10 @@ export class Account extends Component {
     componentDidMount() {
         !sessionStorage.getItem('jwt') && this.props.history.push('/login');
     }
+    handleLogout = () => {
+        sessionStorage.clear();
+        this.props.history.push('/');
+    }
     render() {
         return (
             <div className="account">
@@ -13,32 +17,48 @@ export class Account extends Component {
                     </ul>
                     <ul className="navbar-links-items">
                         <li className="navbar-links-item"><a href="#" className="navbar-link">My profile</a></li>
-                        <li className="navbar-links-item"><a href="#" className="navbar-link">Logout</a></li>
+                        <li className="navbar-links-item"><a href="#" className="navbar-link" onClick={this.handleLogout}>Logout</a></li>
                     </ul>
                 </div>
                 <div className="account-item buy-book">
                     <div className="buy-book-title">
-                        <h1>Buy awesome premium books!</h1>
+                        <h1>Buy premium books!</h1>
                     </div>
-                    <div className="books"></div>
-                </div>
-                <div className="account-item socials">
-                    <div className="social"><i className="icon-facebook fb"></i></div>
-                    <div className="social"><i className="icon-instagram ig"></i></div>
-                    <div className="social"><i className="icon-twitter tw"></i></div>
-                    <div className="social"><i className="icon-youtube-play yt"></i></div>
+                    <div className="buy-books-container">
+                        <div className="book soadventure">
+                            <h4 className="title">So Adventure</h4>
+                            <div className="buy-details">
+                                <h4 className="price">9.99$</h4>
+                                <button className="buy-button">Buy</button>
+                            </div>
+                        </div>
+                        <div className="book waterstory">
+                            <h4 className="title">Water Story</h4>
+                            <div className="buy-details">
+                                <h4 className="price">9.99$</h4>
+                                <button className="buy-button">Buy</button>
+                            </div>
+                        </div>
+                        <div className="book wildnature">
+                            <h4 className="title">Wild Nature</h4>
+                            <div className="buy-details">
+                                <h4 className="price">9.99$</h4>
+                                <button className="buy-button">Buy</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="account-item find-book">
                     <div className="find-book-title">
                         <h1>Find here awesome books!</h1>
                     </div>
                     <div className="find-field">
-                        <input className="find-book-input" name="book" type="text" placeholder="Type title of book" />
+                        <input className="find-book-input" name="book" type="text" placeholder="Type title of book..." />
                         <button className="find-button">Find</button>
                     </div>
                 </div>
                 <div className="account-item books">
-                    <div className="books-container">
+                    <div className="find-books-container">
                         <div className="book crimix">
                             <h4 className="title">Crimix</h4>
                             <button className="borrow-button">Borrow</button>
