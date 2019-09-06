3fd29068587265b392f2665319cf82e82786b6d8
FrontDevJacob <jxtoxic24@gmail.com>
2019-09-06 18:03:02 +0200

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
-import { useSelector } from 'react-redux'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { CookiesProvider } from 'react-cookie'
 
-import Style from './Style/Style'
-import Home from './Home/Home'
+// import Home from './Home/Home'
 import Login from './Login/Login'
-import Register from './Register/Register'
-import Store from './Store/Store'
-import Profile from './Profile/Profile'
-import Cart from './Cart/Cart'
-import BookUploader from '../sharedComponents/BookUploader/BookUploader'
-import Loader from '../sharedComponents/Loader/Loader';
+// import Register from './Register/Register'
+// import Store from './Store/Store'
+// import Profile from './Profile/Profile'
+// import Cart from './Cart/Cart'
 
 const AppWrapper = styled.div``;
 
 const App = () => {
-  const isLoading = useSelector(state => state.global.isLoading)
-  const shouldBookUploaderAppear = useSelector(state => state.global.shouldBookUploaderAppear)
-  return (
-    <CookiesProvider>
-      <AppWrapper>
-        <Style />
-        {shouldBookUploaderAppear && <BookUploader />}
-        {isLoading && <Loader />}
-        <Router>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/login' component={Login} />
-            <Route path='/register' component={Register} />
-            <Route path='/store' component={Store} />
-            <Route path='/profile' component={Profile} />
-            <Route path='/cart' component={Cart} />
-          </Switch>
-        </Router>
-      </AppWrapper>
-    </CookiesProvider>
-  )
+    return (
+        <CookiesProvider>
+            <AppWrapper>
+                <Router>
+                    <Switch>
+                        <Route path='/login' component={Login} />
+                    </Switch>
+                </Router>
+            </AppWrapper>
+        </CookiesProvider>
+    )
 }
 
 export default App
