2fab667e688a8c3e295621913fc3792747d1facf
toxxiczny <jxtoxic24@gmail.com>
2020-07-04 01:17:02 +0200

    add transition for menu padding

--- src/components/UserStore/UserStore.tsx
+++ src/components/UserStore/UserStore.tsx
@@ -14,6 +14,7 @@ const UserStoreContainer = styled(HomeContainer)`
     min-height: ${() => hooks.useHeight()};
     padding: ${({ shouldExpandMenu }: IProps) =>
         shouldExpandMenu ? '344px 0px 20px 0px' : '130px 0px 20px 0px'};
+    transition: padding 0.5s ease-in-out;
     @media (min-width: 800px) {
         padding: 130px 0px 20px 0px;
     }
