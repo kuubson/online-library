b76f7d5c0ce22a25efbb54a37614f40cbb69d49b
toxxiczny <jxtoxic24@gmail.com>
2021-05-09 10:53:21 +0200

    handle push notifications with service worker properly

--- src/service-worker.js
+++ src/service-worker.js
@@ -35,3 +35,20 @@ self.addEventListener('message', event => {
         self.skipWaiting()
     }
 })
+
+self.addEventListener('push', event => {
+    const { title, body, image, icon, data } = event.data.json()
+    event.waitUntil(
+        self.registration.showNotification(title, {
+            body,
+            image,
+            icon,
+            data
+        })
+    )
+})
+
+self.addEventListener('notificationclick', ({ notification }) => {
+    notification.close()
+    clients.openWindow(notification.data.url)
+})
