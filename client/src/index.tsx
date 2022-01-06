4ab9096de6fadd0b1317cba124077a708a6ef1b0
toxxiczny <jxtoxic24@gmail.com>
2022-01-06 20:23:33 +0100

    remove unnecessary dependencies

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import { ThemeProvider } from 'styled-components'
 import { ApolloProvider } from '@apollo/client'
+import { ThemeProvider } from 'styled-components'
 
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 import { store, persistor } from 'redux/store'
+
+import { client } from 'gql/client'
 
 import { theme } from 'assets/styles/theme'
 import 'assets/styles/index.scss'
@@ -16,8 +18,6 @@ import Wrapper from 'components/shared/Wrapper/Wrapper'
 import Loader from 'components/shared/Loader/Loader'
 
 import App from 'components/App'
-
-import { client } from 'gql/client'
 
 declare global {
     interface Window {
@@ -29,13 +29,13 @@ render(
     <StrictMode>
         <Provider store={store}>
             <PersistGate loading={<Loader />} persistor={persistor}>
-                <ThemeProvider theme={theme}>
-                    <ApolloProvider client={client}>
+                <ApolloProvider client={client}>
+                    <ThemeProvider theme={theme}>
                         <Wrapper>
                             <App />
                         </Wrapper>
-                    </ApolloProvider>
-                </ThemeProvider>
+                    </ThemeProvider>
+                </ApolloProvider>
             </PersistGate>
         </Provider>
     </StrictMode>,
