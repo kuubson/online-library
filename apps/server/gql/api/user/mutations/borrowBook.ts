3de70550945a32c8b577a5043a314a52f94676a4
toxxiczny <jxtoxic24@gmail.com>
2022-09-14 13:01:09 +0200

    fix not working gql subscriptions (graphql-shield was blocking it)

--- apps/server/gql/api/user/mutations/borrowBook.ts
+++ apps/server/gql/api/user/mutations/borrowBook.ts
@@ -5,8 +5,6 @@ import { Book } from 'database'
 import type { MutationResolvers } from 'types/graphql'
 
 export const borrowBook: MutationResolvers['borrowBook'] = async (_, { bookId }, { req }) => {
-   // pubsub.publish('user', { user: { name: req.user.user.name } }) // TODO: delete it later
-
    const book = await Book.findByPk(bookId)
 
    if (!book) {
