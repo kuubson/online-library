fdfe5a6f2b4aeb9a8acc689e562c0e735a6c9180
toxxiczny <jxtoxic24@gmail.com>
2022-09-19 23:49:35 +0200

    skip noHoist

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -1,11 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
+const path = require('path')
+
 module.exports = {
+   projectRoot: path.resolve(__dirname, '../../'),
    transformer: {
       getTransformOptions: async () => ({
          transform: {
             experimentalImportSupport: false,
-            inlineRequires: true,
+            inlineRequires: false,
          },
       }),
    },
+   resolver: { blacklistRE: /apps\\server\\/ },
 }
--- apps/native/src/App.tsx
+++ apps/native/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
-import styled from 'styled-components'
+import { Text } from 'react-native'
+import styled from 'styled-components/native'
 
 import { API } from '@online-library/tools'
 
@@ -7,7 +8,11 @@ export const App = () => {
    useEffect(() => {
       console.log(API)
    }, [])
-   return <AppContainer></AppContainer>
+   return (
+      <AppContainer>
+         <Text>Online Library</Text>
+      </AppContainer>
+   )
 }
 
-const AppContainer = styled.div``
+const AppContainer = styled.View``
