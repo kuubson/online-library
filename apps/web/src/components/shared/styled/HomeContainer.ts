f33990623cb01dd378d52bf7e0ea0ff28c9652bd
toxxiczny <jxtoxic24@gmail.com>
2022-09-20 10:45:31 +0200

    move HomeContainer

--- apps/web/src/components/shared/styled/HomeContainer.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components/macro'
-
-export const HomeContainer = styled.section`
-   width: 100%;
-   display: flex;
-   justify-content: space-around;
-   align-items: center;
-`
