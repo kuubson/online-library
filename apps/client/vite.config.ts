cf7dfed328a63af9044b8a954e9d07744adf2d46
kuubson <jxtoxic24@gmail.com>
2022-11-21 20:11:49 +0100

    tweak vite boilerplate

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,7 +1,8 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [react()],
+   plugins: [react({ babel: { plugins: [['babel-plugin-styled-components']] } }), tsconfigPaths()],
+   server: { port: 3000 },
 })
