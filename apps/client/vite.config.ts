dfc4def06efb6528207c7ba3d050a69b2df1d95b
kuubson <jxtoxic24@gmail.com>
2022-11-21 20:51:05 +0100

    handle pwa

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,8 +1,38 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-   plugins: [react({ babel: { plugins: [['babel-plugin-styled-components']] } }), tsconfigPaths()],
    server: { port: 3000 },
+   plugins: [
+      react({
+         babel: { plugins: [['babel-plugin-styled-components']] },
+      }),
+      VitePWA({
+         includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+         devOptions: { enabled: true },
+         manifest: {
+            short_name: 'Online Library',
+            name: 'Online Library',
+            description: 'Online Library',
+            theme_color: '#0088ff',
+            background_color: 'white',
+            icons: [
+               {
+                  src: '/favicons/android-chrome-192x192.png',
+                  sizes: '192x192',
+                  type: 'image/png',
+               },
+               {
+                  src: '/favicons/android-chrome-512x512.png',
+                  sizes: '512x512',
+                  type: 'image/png',
+               },
+            ],
+         },
+         registerType: 'prompt',
+      }),
+      tsconfigPaths(),
+   ],
 })
